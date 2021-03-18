a10226a8e72854aa3621eb16d588847e16bc96b5
nathalyusl <nathalyusl@gmail.com>
2021-03-17 23:26:45 -0500

    Contact

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -22,3 +22,14 @@
   /**
    * Easy event listener function
    */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
