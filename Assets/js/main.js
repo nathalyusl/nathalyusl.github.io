d89b3b8a0e8a95f212ed4c456e1349dc94a7f680
nathalyusl <nathalyusl@gmail.com>
2021-03-17 23:24:21 -0500

    Contact

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -6,3 +6,14 @@
  */
 (function() {
     "use strict";
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
