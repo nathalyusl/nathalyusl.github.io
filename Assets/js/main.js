b1bc048e09572daee52d63ac29dd14b9463c8585
nathalyusl <nathalyusl@gmail.com>
2021-03-18 20:37:07 -0500

    Mobile app

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -50,4 +50,5 @@
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
   }
