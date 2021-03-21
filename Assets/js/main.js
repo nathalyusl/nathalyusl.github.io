4e354e2037a8de17ed66c825854a329c1092e891
nathalyusl <nathalyusl@gmail.com>
2021-03-20 23:45:59 -0500

    Mobile app

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -195,3 +195,10 @@
       });
 
       let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
